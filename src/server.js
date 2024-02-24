6fea561d93f0d00a8be76bd38c1479a24dc4b20c
manfrim <rafaelmanfrim2004@gmail.com>
2024-02-24 12:53:58 -0300

    feat: adiciona armazenamento em memória para usuários, criação e listagem

--- src/server.js
+++ src/server.js
@@ -1,7 +1,27 @@
 import http from 'http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
-  return res.end("Hello World!")
+  const { method, url } = req
+
+  if (method === 'GET' && url == "/users") {
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url == "/users") {
+    users.push({
+      id: 1,
+      name: "Jhon Doe",
+      email: "jhon.doe@gmail.com"
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
