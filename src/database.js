3d5faf9ba5f542ede6970465cc1c0fa12b91fb74
manfrim <rafaelmanfrim2004@gmail.com>
2025-01-03 20:46:25 -0300

    Persistindo banco de dados em arquivo JSON

--- src/database.js
+++ src/database.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs/promises';
+
+const databasePath = new URL("../db.json", import.meta.url)
+
 export class Database {
   #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'utf8').then(
+      data => this.#database = JSON.parse(data)
+    ).catch(_ => this.#persist())
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   select(table) {
     const data = this.#database[table] ?? []
@@ -12,5 +26,9 @@ export class Database {
     }
 
     this.#database[table].push(data)
+
+    this.#persist()
+
+    return data
   }
 }
